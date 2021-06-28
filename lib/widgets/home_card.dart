198f7e5bfb6478b7c24511e2019c2e1489cbeb03
Ariz Armeidi <arizarmeidi@gmail.com>
2021-06-28 11:48:26 +0700

    Update home_card.dart

--- lib/widgets/home_card.dart
+++ lib/widgets/home_card.dart
@@ -15,43 +15,48 @@ class HomeCard extends StatelessWidget {
     // final device = MediaQuery.of(context);
     // final screenHeight = device.size.height;
     // final screenWidth = device.size.width;
-    return Column(
-      children: [
-        Expanded(
-          child: ClipRRect(
-            borderRadius: BorderRadius.circular(10),
-            child: Image.network(
-              homeData.imageUrl,
-              fit: BoxFit.cover,
+    return InkWell(
+      onTap: () {
+        print(homeData.title);
+      },
+      child: Column(
+        children: [
+          Expanded(
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(10),
+              child: Image.network(
+                homeData.imageUrl,
+                fit: BoxFit.cover,
+              ),
             ),
           ),
-        ),
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 15),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Flexible(
-                child: Text(
-                  homeData.title,
-                  maxLines: 2,
-                  overflow: TextOverflow.ellipsis,
-                  style: TextStyle(
-                    fontWeight: FontWeight.w700,
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 15),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Flexible(
+                  child: Text(
+                    homeData.title,
+                    maxLines: 2,
+                    overflow: TextOverflow.ellipsis,
+                    style: TextStyle(
+                      fontWeight: FontWeight.w700,
+                    ),
                   ),
                 ),
-              ),
-              IconButton(
-                icon: Icon(
-                  Icons.bookmark_outline,
-                  color: Colors.orangeAccent,
+                IconButton(
+                  icon: Icon(
+                    Icons.bookmark_outline,
+                    color: Colors.orangeAccent,
+                  ),
+                  onPressed: () {},
                 ),
-                onPressed: () {},
-              ),
-            ],
-          ),
-        )
-      ],
+              ],
+            ),
+          )
+        ],
+      ),
     );
   }
 }
